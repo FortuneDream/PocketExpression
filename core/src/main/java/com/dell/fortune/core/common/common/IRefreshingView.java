40ef21cb08044705ac8b4ac2439f2375bb1b26f8
FortuenDream <812568684@qq.com>
2018-06-23 11:21:42 +0800

    删除的文件

--- core/src/main/java/com/dell/fortune/core/common/common/IRefreshingView.java
+++
@@ -1,9 +0,0 @@
-package com.dell.fortune.pocketexpression.common;
-
-/**
- * Created by 81256 on 2018/4/11.
- */
-
-public interface IRefreshingView {
-    public void showRefreshing(boolean isShow);
-}
